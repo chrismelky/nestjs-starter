37e49cb77d7fb8dd5055dad0bde2b5dbcbfc732b
Chrispinu Melkiory <chrismelky05@gmail.com>
2022-06-22 17:52:02 +0300

    fix typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,36 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Inject, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { logger } from './logger.middleware';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { ErrorHandlerFilter } from './error-handler.filter';
-import { getConnectionOptions } from 'typeorm';
 import { UserModule } from './modules/user/user.module';
 import { RoleModule } from './modules/role/role.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtGuard } from './auth/jwt.guard';
 import { AuthorityGuard } from './auth/authority.guard';
 import { MenuModule } from './modules/menu/menu.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        Object.assign(await getConnectionOptions(), {
-          autoLoadEntities: true,
-        }),
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: +config.get('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_DATABASE'),
+        synchronize: config.get('DB_SYNCHRONIZE'),
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
     RoleModule,
